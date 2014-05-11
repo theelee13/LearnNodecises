5d3253fe80f3db2e2ba0bcc74b690b86fe6cbf35
Edward Lee <theelee13@gmail.com>
2014-05-11 17:52:55 -0400

    Added comments to exercise 8

--- ex8.js
+++ ex8.js
@@ -3,10 +3,10 @@ var http = require('http');
 http.get(process.argv[2], function (res){
 	res.setEncoding('utf8');
 	res.on('data',function (d2){
-		data+=d2;
+		data+=d2;	//adds each piece of data sent to the data string.
 	});
 	res.on('end',function(){
-		console.log(data.length);
-		console.log(data);
+		console.log(data.length);	//assignment asks for length of data
+		console.log(data);			//print all data sent through get.
 	});
 });
