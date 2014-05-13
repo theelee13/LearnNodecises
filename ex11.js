06ca088339cdab60594c53917c7942cf2f8a3db1
Edward Lee <theelee13@gmail.com>
2014-05-13 13:14:40 -0400

    Added comments for exercise 11.

--- ex11.js
+++ ex11.js
@@ -1,8 +1,8 @@
 var http = require('http');
 var fs = require('fs');
 var server = http.createServer(function (req,res){
-	res.writeHeader(200,{'Content-Type':'text/plain'});
-	var fStream = fs.createReadStream(process.argv[3]);
-	fStream.pipe(res);
+	res.writeHeader(200,{'Content-Type':'text/plain'});	//http code 200 for successful connection and response.
+	var fStream = fs.createReadStream(process.argv[3]);	//create Stream object with designated url for .txt file
+	fStream.pipe(res);												//Pipe the file stream to the res variable
 });
-server.listen(process.argv[2]);
+server.listen(process.argv[2]);	//obviously require some listening.
